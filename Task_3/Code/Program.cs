a2f80f8cde1327a29bbb661170270c4720f4e630
AndriyPalamar1 <108981219+AndriyPalamar1@users.noreply.github.com>
2023-10-09 20:14:07 +0300

    Add files via upload

---
+++ Task_3/Code/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelpArmyDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //DB_connect db_connect = new DB_connect();
+            //db_connect.SetupingScript();
+            TablesDB tablesDB = new TablesDB();
+            tablesDB.FillTables();
+            tablesDB.PrintAllTables();
+        }
+    }
+}
